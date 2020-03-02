150c00dd59cded307badf477fe0b1291bdd95a86
Krystian Panek <krystian.panek@cognifide.com>
2020-03-02 13:49:12 +0100

    Minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("com.cognifide.gradle:common-plugin:0.1.18")
+    implementation("com.cognifide.gradle:common-plugin:0.1.19")
     implementation("org.buildobjects:jproc:2.2.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
     implementation("org.apache.commons:commons-lang3:3.9")
