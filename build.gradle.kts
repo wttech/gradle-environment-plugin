0c25722679398a5bc537251637fb9a7e80639080
Krystian Panek <krystian.panek@cognifide.com>
2020-04-15 10:41:16 +0200

    Version bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,12 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("com.cognifide.gradle:common-plugin:0.1.44")
+    implementation("com.cognifide.gradle:common-plugin:0.1.45")
     implementation("org.buildobjects:jproc:2.3.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
     implementation("org.apache.commons:commons-lang3:3.9")
     implementation("commons-io:commons-io:2.6")
-    implementation("org.apache.httpcomponents:httpclient:4.5.10")
+    implementation("org.apache.httpcomponents:httpclient:4.5.12")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
 
