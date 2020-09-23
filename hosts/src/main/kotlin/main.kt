14a1c9b73531ca9d99073ba2ed6161eb6055f941
Krystian Panek <krystian.panek@cognifide.com>
2020-09-23 09:35:42 +0200

    Minors

--- hosts/src/main/kotlin/main.kt
+++ hosts/src/main/kotlin/main.kt
@@ -1,14 +1,20 @@
 import java.io.File
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) = try {
-    val sourceName = args.getOrNull(0) ?: "mysite"
-    val sourceFile = File(args.getOrNull(1) ?: ".gradle/environment/hosts.txt")
-    val sourceSection = Section(sourceName, sourceFile.readText().lines().map { it.trim() })
-    val targetFile = File(args.getOrNull(2) ?: "/etc/hosts")
+    if (args.size != 3) {
+        println("Invalid arguments! Expected: [sectionName] [sectionEntriesFile] [hostsFile]")
+        exitProcess(1)
+    }
+
+    val sourceName = args[0]
+    val sourceFile = File(args[1])
+    val targetFile = File(args[2])
 
     val text = targetFile.readText()
     val sections = Section.parseAll(text)
 
+    val sourceSection = Section(sourceName, sourceFile.readText().lines().map { it.trim() })
     val targetSection = sections.find { it.name == sourceSection.name }
 
     if (targetSection != null) {
@@ -20,6 +26,7 @@ fun main(args: Array<String>) = try {
     println("Cannot update hosts file!")
     println("Ensure using administrator/super-user privileges.")
     println("Error details: ${e.message}")
+    exitProcess(1)
 }
 
 data class Section(val name: String, val entries: List<String>) {
